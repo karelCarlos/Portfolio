fd6fd22b8aff8c1bb304ba5d10971306d577393f
Karel Carlos <150103573+karelCarlos@users.noreply.github.com>
2023-11-26 21:06:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+   // Get the button element
+   var scrollTop= document.getElementById("scrollTop");
+
+   // When the user scrolls down 20px from the top, show the button
+   window.onscroll = function() {
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+           scrollTop.style.display = "block";
+       } else {
+           scrollTop.style.display = "none";
+       }
+   };
+
+   // When the user clicks on the button, scroll to the top of the document
+   
